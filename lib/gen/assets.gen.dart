6a482ea5fe7e88bf619a0e4a696338d491a8bd26
AbdelmoneamIsmael <asa.abdo5@gmail.com>
2025-08-07 12:00:01 +0300

    first commit

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,450 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/Cairo-Black.ttf
+  String get cairoBlack => 'assets/fonts/Cairo-Black.ttf';
+
+  /// File path: assets/fonts/Cairo-Bold.ttf
+  String get cairoBold => 'assets/fonts/Cairo-Bold.ttf';
+
+  /// File path: assets/fonts/Cairo-ExtraBold.ttf
+  String get cairoExtraBold => 'assets/fonts/Cairo-ExtraBold.ttf';
+
+  /// File path: assets/fonts/Cairo-ExtraLight.ttf
+  String get cairoExtraLight => 'assets/fonts/Cairo-ExtraLight.ttf';
+
+  /// File path: assets/fonts/Cairo-Light.ttf
+  String get cairoLight => 'assets/fonts/Cairo-Light.ttf';
+
+  /// File path: assets/fonts/Cairo-Medium.ttf
+  String get cairoMedium => 'assets/fonts/Cairo-Medium.ttf';
+
+  /// File path: assets/fonts/Cairo-Regular.ttf
+  String get cairoRegular => 'assets/fonts/Cairo-Regular.ttf';
+
+  /// File path: assets/fonts/Cairo-SemiBold.ttf
+  String get cairoSemiBold => 'assets/fonts/Cairo-SemiBold.ttf';
+
+  /// File path: assets/fonts/Tajawal-Black.ttf
+  String get tajawalBlack => 'assets/fonts/Tajawal-Black.ttf';
+
+  /// File path: assets/fonts/Tajawal-Bold.ttf
+  String get tajawalBold => 'assets/fonts/Tajawal-Bold.ttf';
+
+  /// File path: assets/fonts/Tajawal-ExtraBold.ttf
+  String get tajawalExtraBold => 'assets/fonts/Tajawal-ExtraBold.ttf';
+
+  /// File path: assets/fonts/Tajawal-ExtraLight.ttf
+  String get tajawalExtraLight => 'assets/fonts/Tajawal-ExtraLight.ttf';
+
+  /// File path: assets/fonts/Tajawal-Light.ttf
+  String get tajawalLight => 'assets/fonts/Tajawal-Light.ttf';
+
+  /// File path: assets/fonts/Tajawal-Medium.ttf
+  String get tajawalMedium => 'assets/fonts/Tajawal-Medium.ttf';
+
+  /// File path: assets/fonts/Tajawal-Regular.ttf
+  String get tajawalRegular => 'assets/fonts/Tajawal-Regular.ttf';
+
+  /// List of all assets
+  List<String> get values => [
+        cairoBlack,
+        cairoBold,
+        cairoExtraBold,
+        cairoExtraLight,
+        cairoLight,
+        cairoMedium,
+        cairoRegular,
+        cairoSemiBold,
+        tajawalBlack,
+        tajawalBold,
+        tajawalExtraBold,
+        tajawalExtraLight,
+        tajawalLight,
+        tajawalMedium,
+        tajawalRegular
+      ];
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/Medicines Bowl.svg
+  String get medicinesBowl => 'assets/icons/Medicines Bowl.svg';
+
+  /// File path: assets/icons/SHOBIC.svg
+  String get shobic => 'assets/icons/SHOBIC.svg';
+
+  /// File path: assets/icons/Shbek Lbek+.svg
+  String get shbekLbek => 'assets/icons/Shbek Lbek+.svg';
+
+  /// File path: assets/icons/Stethoscope Medical .svg
+  String get stethoscopeMedical => 'assets/icons/Stethoscope Medical .svg';
+
+  /// File path: assets/icons/about.svg
+  String get about => 'assets/icons/about.svg';
+
+  /// File path: assets/icons/ad_svg.svg
+  String get adSvg => 'assets/icons/ad_svg.svg';
+
+  /// File path: assets/icons/ask_question.svg
+  String get askQuestion => 'assets/icons/ask_question.svg';
+
+  /// File path: assets/icons/balance.svg
+  String get balance => 'assets/icons/balance.svg';
+
+  /// File path: assets/icons/card_background.svg
+  String get cardBackground => 'assets/icons/card_background.svg';
+
+  /// File path: assets/icons/chat.svg
+  String get chat => 'assets/icons/chat.svg';
+
+  /// File path: assets/icons/check_box.svg
+  String get checkBox => 'assets/icons/check_box.svg';
+
+  /// File path: assets/icons/chevron-down.svg
+  String get chevronDown => 'assets/icons/chevron-down.svg';
+
+  /// File path: assets/icons/communication.svg
+  String get communication => 'assets/icons/communication.svg';
+
+  /// File path: assets/icons/dashboard.svg
+  String get dashboard => 'assets/icons/dashboard.svg';
+
+  /// File path: assets/icons/delete.svg
+  String get delete => 'assets/icons/delete.svg';
+
+  /// File path: assets/icons/edit_icon.svg
+  String get editIcon => 'assets/icons/edit_icon.svg';
+
+  /// File path: assets/icons/emptyPage.svg
+  String get emptyPage => 'assets/icons/emptyPage.svg';
+
+  /// File path: assets/icons/expenses.svg
+  String get expenses => 'assets/icons/expenses.svg';
+
+  /// File path: assets/icons/feeatures.svg
+  String get feeatures => 'assets/icons/feeatures.svg';
+
+  /// File path: assets/icons/home.svg
+  String get home => 'assets/icons/home.svg';
+
+  /// File path: assets/icons/income.svg
+  String get income => 'assets/icons/income.svg';
+
+  /// File path: assets/icons/josua_nunito.svg
+  String get josuaNunito => 'assets/icons/josua_nunito.svg';
+
+  /// File path: assets/icons/language-svgrepo-com.svg
+  String get languageSvgrepoCom => 'assets/icons/language-svgrepo-com.svg';
+
+  /// File path: assets/icons/left_back_icon.svg
+  String get leftBackIcon => 'assets/icons/left_back_icon.svg';
+
+  /// File path: assets/icons/lekan.svg
+  String get lekan => 'assets/icons/lekan.svg';
+
+  /// File path: assets/icons/location.svg
+  String get location => 'assets/icons/location.svg';
+
+  /// File path: assets/icons/logout.svg
+  String get logout => 'assets/icons/logout.svg';
+
+  /// File path: assets/icons/madrani_andi.svg
+  String get madraniAndi => 'assets/icons/madrani_andi.svg';
+
+  /// File path: assets/icons/my_card_icon.svg
+  String get myCardIcon => 'assets/icons/my_card_icon.svg';
+
+  /// File path: assets/icons/my_investments.svg
+  String get myInvestments => 'assets/icons/my_investments.svg';
+
+  /// File path: assets/icons/my_transaction.svg
+  String get myTransaction => 'assets/icons/my_transaction.svg';
+
+  /// File path: assets/icons/noJopIcon.svg
+  String get noJopIcon => 'assets/icons/noJopIcon.svg';
+
+  /// File path: assets/icons/no_notification.svg
+  String get noNotification => 'assets/icons/no_notification.svg';
+
+  /// File path: assets/icons/no_requests.svg
+  String get noRequests => 'assets/icons/no_requests.svg';
+
+  /// File path: assets/icons/no_user.svg
+  String get noUser => 'assets/icons/no_user.svg';
+
+  /// File path: assets/icons/pill.svg
+  String get pill => 'assets/icons/pill.svg';
+
+  /// File path: assets/icons/privacy.svg
+  String get privacy => 'assets/icons/privacy.svg';
+
+  /// File path: assets/icons/profile.svg
+  String get profile => 'assets/icons/profile.svg';
+
+  /// File path: assets/icons/rate_us.svg
+  String get rateUs => 'assets/icons/rate_us.svg';
+
+  /// File path: assets/icons/report_bug.svg
+  String get reportBug => 'assets/icons/report_bug.svg';
+
+  /// File path: assets/icons/report_post.svg
+  String get reportPost => 'assets/icons/report_post.svg';
+
+  /// File path: assets/icons/save.svg
+  String get save => 'assets/icons/save.svg';
+
+  /// File path: assets/icons/search.svg
+  String get search => 'assets/icons/search.svg';
+
+  /// File path: assets/icons/setting_system.svg
+  String get settingSystem => 'assets/icons/setting_system.svg';
+
+  /// File path: assets/icons/settings.svg
+  String get settings => 'assets/icons/settings.svg';
+
+  /// File path: assets/icons/share.svg
+  String get share => 'assets/icons/share.svg';
+
+  /// File path: assets/icons/snackbar_fail.svg
+  String get snackbarFail => 'assets/icons/snackbar_fail.svg';
+
+  /// File path: assets/icons/snackbar_info.svg
+  String get snackbarInfo => 'assets/icons/snackbar_info.svg';
+
+  /// File path: assets/icons/snackbar_success.svg
+  String get snackbarSuccess => 'assets/icons/snackbar_success.svg';
+
+  /// File path: assets/icons/snackbar_warning.svg
+  String get snackbarWarning => 'assets/icons/snackbar_warning.svg';
+
+  /// File path: assets/icons/statistic.svg
+  String get statistic => 'assets/icons/statistic.svg';
+
+  /// File path: assets/icons/support.svg
+  String get support => 'assets/icons/support.svg';
+
+  /// File path: assets/icons/teeth.svg
+  String get teeth => 'assets/icons/teeth.svg';
+
+  /// File path: assets/icons/terms.svg
+  String get terms => 'assets/icons/terms.svg';
+
+  /// File path: assets/icons/theme.svg
+  String get theme => 'assets/icons/theme.svg';
+
+  /// File path: assets/icons/uncheck_box.svg
+  String get uncheckBox => 'assets/icons/uncheck_box.svg';
+
+  /// File path: assets/icons/uploadIcon.svg
+  String get uploadIcon => 'assets/icons/uploadIcon.svg';
+
+  /// File path: assets/icons/uploadIcon2.svg
+  String get uploadIcon2 => 'assets/icons/uploadIcon2.svg';
+
+  /// File path: assets/icons/wallet_account.svg
+  String get walletAccount => 'assets/icons/wallet_account.svg';
+
+  /// File path: assets/icons/works.svg
+  String get works => 'assets/icons/works.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        medicinesBowl,
+        shobic,
+        shbekLbek,
+        stethoscopeMedical,
+        about,
+        adSvg,
+        askQuestion,
+        balance,
+        cardBackground,
+        chat,
+        checkBox,
+        chevronDown,
+        communication,
+        dashboard,
+        delete,
+        editIcon,
+        emptyPage,
+        expenses,
+        feeatures,
+        home,
+        income,
+        josuaNunito,
+        languageSvgrepoCom,
+        leftBackIcon,
+        lekan,
+        location,
+        logout,
+        madraniAndi,
+        myCardIcon,
+        myInvestments,
+        myTransaction,
+        noJopIcon,
+        noNotification,
+        noRequests,
+        noUser,
+        pill,
+        privacy,
+        profile,
+        rateUs,
+        reportBug,
+        reportPost,
+        save,
+        search,
+        settingSystem,
+        settings,
+        share,
+        snackbarFail,
+        snackbarInfo,
+        snackbarSuccess,
+        snackbarWarning,
+        statistic,
+        support,
+        teeth,
+        terms,
+        theme,
+        uncheckBox,
+        uploadIcon,
+        uploadIcon2,
+        walletAccount,
+        works
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/8208952.jpg
+  AssetGenImage get a8208952 =>
+      const AssetGenImage('assets/images/8208952.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [a8208952];
+}
+
+class $AssetsLottieGen {
+  const $AssetsLottieGen();
+
+  /// File path: assets/lottie/alert.json
+  String get alert => 'assets/lottie/alert.json';
+
+  /// File path: assets/lottie/intro screen.json
+  String get introScreen => 'assets/lottie/intro screen.json';
+
+  /// File path: assets/lottie/notification.json
+  String get notification => 'assets/lottie/notification.json';
+
+  /// File path: assets/lottie/order_confirmed.json
+  String get orderConfirmed => 'assets/lottie/order_confirmed.json';
+
+  /// File path: assets/lottie/orders.json
+  String get orders => 'assets/lottie/orders.json';
+
+  /// List of all assets
+  List<String> get values =>
+      [alert, introScreen, notification, orderConfirmed, orders];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLottieGen lottie = $AssetsLottieGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
